79368ce76bda64c4834c8e24561f409bc8f018b9
synergistics <kingjak678@gmail.com>
2016-06-09 23:06:26 +0000

    Adds barebones client server communication

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var WebSocketServer = require('websocket').server;
 var serveStatic = require('serve-static');
 
 var serve = serveStatic('public', {index: 'index.html'});
@@ -6,5 +7,18 @@ var serve = serveStatic('public', {index: 'index.html'});
 var server = http.createServer(function(req, res){
   serve(req, res, () => res.end());
 });
-
 server.listen(3000);
+
+var wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+wsServer.on('request', function(request){
+  console.log((new Date()) + ": Recieved request");
+  
+  var connection = request.accept('hi-protocol', request.origin);
+  connection.on('message', function(message){
+    connection.send(message.utf8Data+'!!!!'); 
+  });
+});
+
