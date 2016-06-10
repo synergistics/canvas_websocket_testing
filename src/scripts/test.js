d0e04a0c5aaa3c1f6b8a0eb94ef00860b8f3e1f1
synergistics <kingjak678@gmail.com>
2016-06-10 23:52:17 +0000

    Adds src directory

---
+++ src/scripts/test.js
@@ -0,0 +1,11 @@
+var socket = new WebSocket('ws://localhost:3000', 'hi-protocol');
+
+socket.onopen = function(event){
+  console.log(event);
+
+  socket.send('HI SERVER!!!');
+};
+
+socket.onmessage = function(message){
+  console.log(message);
+}
